52fcb65e1842216bb908c47f8e8f32ea3444fa27
Rmurthy1 <Rahul@Rahuls-MacBook-Air.local>
2015-05-30 05:57:54 -0700

    frequently seen
    works atm, gonna clear up the structs next.

--- downloadAndStore.cpp
+++ downloadAndStore.cpp
@@ -21,8 +21,9 @@ using std::string;
 #include <curl/curl.h>
 #include <curl/easy.h>
 #include <fstream>
+#include <queue>
 
-
+#include <unistd.h>
 
 // top level
 // collection of matches (aggregate calcs performed here)
@@ -33,10 +34,19 @@ using std::string;
 // low level
 // player data for a single match
 
+class MyGames;
+class Game;
+
+// maybe get rid of some of these friendships
 
 
+/**
+ * class to hold each player's data
+ */
 class Player
 {
+	friend class Game;
+	friend class MyGames;
 	string m_account_id;
 	string m_player_slot;
 	string m_hero_id;
@@ -44,7 +54,16 @@ class Player
 public:
 	Player(string account_id, string player_slot, string hero_id);
 	string getAccountId() {return m_account_id;}
-
+	
+	bool operator<(const Player& rhs) const
+	{
+		int comp = m_account_id.compare(rhs.m_account_id);
+		if (comp < 0)
+		{
+			return true;
+		}
+		return false;
+	}
 };
 
 Player::Player(string account_id, string player_slot, string hero_id)
@@ -54,8 +73,27 @@ Player::Player(string account_id, string player_slot, string hero_id)
 	m_hero_id = hero_id;
 }
 
+
+class comparisonForFrequency
+{
+public:
+	bool operator ()(const std::pair<Player, int> &a, const std::pair<Player, int> &b) const
+	{
+		if (a.second < b.second)
+		{
+			return true;
+		}
+		return false;
+	}
+};
+
+
+/**
+ * class to hold each game's data (includes a collection of players)
+ */
 class Game
 {
+	friend class MyGames;
 	//bool dire_win;
 	//bool did_i_win;
 	string m_lobby_type;
@@ -74,23 +112,90 @@ Game::Game(std::vector<Player> players, string lobby_type, string match_id, stri
 	m_start_time = start_time;
 }
 
-
+/**
+ * class to hold a player's game history
+ */
 class MyGames
 {
 	std::vector<Game> m_allGames;
+	bool compareForPlayerFrequency(const std::pair<Player, int>&, const std::pair<Player, int>&) const;
+	comparisonForFrequency *comp;
 public:
 	void addGames(Game);
-	//void load(const std::string &filename, ostream &out);
+	void load(const std::string &filename);
 	void mostSeen();
+
 };
+
+
+
+bool MyGames::compareForPlayerFrequency(const std::pair<Player, int> &a, const std::pair<Player, int> &b) const
+{
+	if (a.second > b.second)
+	{
+		return true;
+	}
+	return false;
+}
 
 
 void MyGames::mostSeen()
 {
-	for (std::vector<Game>::iterator i = m_allGames.begin(); i != m_allGames.end(); ++i)
+	std::map<Player, int> seenPlayers;
+	
+	for (int i = 0; i < m_allGames.size(); i++)
 	{
-		
+		for (int j = 0; j < m_allGames[i].m_players_in_game.size(); j++)
+		{
+			
+			//std::cout << "writing alot of lines!!!" << std::endl; // test
+			
+			// seenPlayers[m_allGames[i].m_players_in_game[j]]++;
+
+			// swap this count with the .find(key) and see if its not equal to .end() later!!!
+			// otherwise im just doing double the work kinda
+			Player key = m_allGames[i].m_players_in_game[j];
+			if (seenPlayers.count(key) == 1) 
+			{
+                seenPlayers[key] = seenPlayers[key] + 1;
+            }
+            else 
+            {
+                seenPlayers[key] = 1;
+            }
+
+		}
 	}
+	// map now populated with counts
+	//typedef std::map<std::string, std::map<std::string, std::string>>::iterator it_type;
+	/*
+	for (std::map<Player, int>::iterator i = seenPlayers.begin(); i != seenPlayers.end(); i++)
+	{
+		//std::std::vector<pair<> v;
+		//lets just print the map
+		std::cout << "element: " << i->first << " count: " << i->second << std::endl;
+
+	}
+	*/
+	// whole lotta copying going on here, gotta switch this up later
+		// in this im making a pair and then throwing the pair in a priorty queue with a custom compare
+	std::priority_queue<std::pair<Player, int>, std::vector<std::pair<Player, int> >, comparisonForFrequency> pqueue;
+	for (std::map<Player,int>::iterator it=seenPlayers.begin(); it!=seenPlayers.end(); ++it)
+	{
+    	//std::cout << it->first.m_account_id << " => " << it->second << '\n';
+
+    	//std::pair<Player, int> tempPair = *it;
+    	pqueue.push(*it);//maybe switch to &
+
+    }
+
+    for (int i = 0; i < 20; i++)
+    {
+    	std::cout << pqueue.top().first.m_account_id << "=>" << pqueue.top().second << std::endl;
+    	pqueue.pop();
+    }
+
+
 }
 
 void MyGames::addGames(Game g)
@@ -99,9 +204,13 @@ void MyGames::addGames(Game g)
 }
 
 // this is for later!! once things get cleaned up
+// need to add some more fields to MyGames to make this work, also some accessors for those fields
+// at first glance i need
+	// currentOldestMatch
+	// matchesLeft
 
 /*
-void MyGames::load(const std::string &filename, ostream &out)
+void MyGames::load(const std::string &filename)
 {
 	using boost::property_tree::ptree;
 	ptree pt;
@@ -111,16 +220,9 @@ void MyGames::load(const std::string &filename, ostream &out)
 	
 	BOOST_FOREACH (boost::property_tree::ptree::value_type& result, pt)
 	{
-		matchesLeft = result.second.get<std::string>("results_remaining");
-		cout << "there are " << matchesLeft << " matches left" << endl;
+		cout << "there are " << result.second.get<std::string>("results_remaining") << " matches left" << endl;
 		BOOST_FOREACH (boost::property_tree::ptree::value_type& matches, result.second.get_child("matches"))
 		{
-			match tempMatch;
-
-			tempMatch.startTime = matches.second.get<std::string>("start_time");
-			tempMatch.matchID = matches.second.get<std::string>("match_id");
-			tempMatch.match_seq_num = matches.second.get<std::string>("match_seq_num");
-
 			// inside of each match, make a vector of players
 			std::vector<Player> tempPlayerVec; // may need to make this a player pointer, idk
 
@@ -130,24 +232,29 @@ void MyGames::load(const std::string &filename, ostream &out)
 				tempPlayer.account_id = players.second.get<std::string>("account_id");
 				tempPlayer.player_slot = players.second.get<std::string>("player_slot");
 				tempPlayer.heroID = players.second.get<std::string>("hero_id");
+				tempMatch.players.push_back(tempPlayer);
 
 				// remove the _2 when the structs are removed
 				Player tempPlayer_2(players.second.get<std::string>("account_id"), players.second.get<std::string>("player_slot"), players.second.get<std::string>("hero_id"));
-				tempMatch.players.push_back(tempPlayer);
-				tempPlayerVec.push_back(tempPlayer_2);
+				tempPlayerVec.push_back(tempPlayer_2); // add the player to the vector of players
+
+
 				//out << tempMatch.matchID << "," << tempMatch.startTime << "," << tempMatch.match_seq_num << "," << tempPlayer.account_id << "," << tempPlayer.player_slot << "," << tempPlayer.heroID << std::endl;
 				
 			}
-			
+			// temp game
+			Game tempGame(tempPlayerVec, matches.second.get<std::string>("lobby_type"), matches.second.get<std::string>("match_id"), matches.second.get<std::string>("start_time"));
+
 			currentOldestMatch = tempMatch.matchID;
 			matchList.push_back(tempMatch);
+			gameList.addGames(tempGame); // add the match to the collection of matches
 		
 		}
 	}
 	
 }
-
 */
+
 
 struct dotaMatch
 {	
@@ -168,7 +275,7 @@ struct dotaMatch
 
 };
 
-struct player
+struct player_old
 {
 	std::string account_id;
 	std::string player_slot;
@@ -177,7 +284,7 @@ struct player
 
 struct match
 {
-	std::vector<player> players;
+	std::vector<player_old> players;
 	std::string matchID;
 	std::string startTime;
 	std::string match_seq_num;
@@ -223,15 +330,17 @@ void playersMatches::load(const std::string &filename, ostream &out, MyGames &ga
 
 			BOOST_FOREACH (boost::property_tree::ptree::value_type& players, matches.second.get_child("players"))
 			{
-				player tempPlayer; // struct (old way)
+				player_old tempPlayer; // struct (old way)
 				tempPlayer.account_id = players.second.get<std::string>("account_id");
 				tempPlayer.player_slot = players.second.get<std::string>("player_slot");
 				tempPlayer.heroID = players.second.get<std::string>("hero_id");
+				tempMatch.players.push_back(tempPlayer);
 
 				// remove the _2 when the structs are removed
 				Player tempPlayer_2(players.second.get<std::string>("account_id"), players.second.get<std::string>("player_slot"), players.second.get<std::string>("hero_id"));
-				tempMatch.players.push_back(tempPlayer);
-				tempPlayerVec.push_back(tempPlayer_2);
+				tempPlayerVec.push_back(tempPlayer_2); // add the player to the vector of players
+
+
 				//out << tempMatch.matchID << "," << tempMatch.startTime << "," << tempMatch.match_seq_num << "," << tempPlayer.account_id << "," << tempPlayer.player_slot << "," << tempPlayer.heroID << std::endl;
 				
 			}
@@ -240,6 +349,7 @@ void playersMatches::load(const std::string &filename, ostream &out, MyGames &ga
 
 			currentOldestMatch = tempMatch.matchID;
 			matchList.push_back(tempMatch);
+			gameList.addGames(tempGame); // add the match to the collection of matches
 		
 		}
 	}
@@ -328,16 +438,17 @@ int main()
 
     	// prepare first string
 
-    	int skip = 5;
 
 
     	while (continueToFetch)
 		{
+			// wait for one second before hammering the servers
+			usleep(1000000);
+
 			CURL *curl;
     		FILE *fp;
     		CURLcode res;
 			cout << useString << endl;
-			skip--;
 			const char *url = useString.c_str();
 			char outfilename[FILENAME_MAX] = "test.json";
 
@@ -352,11 +463,10 @@ int main()
 				res = curl_easy_perform(curl);
 				curl_easy_cleanup(curl);
 				fclose(fp);
-				
+
 				// parse file
 				
 				rec.load("test.json", fout, games);
-
 
 				char tempString[50];
 				
@@ -365,22 +475,11 @@ int main()
  				if(rec.matchesLeft == "0")
 				{
 					//continueToFetch = false;
-					cout << "it says there are none left! im going to keep on going" << endl;
+					cout << "it says there are none left! im going to quit!" << endl;
 					break;
-					// this is stuff copied from below
-					amountOfMatchesToFetch = "100";
-					startFromPosition = rec.currentOldestMatch;
-					int value = ((atoi(startFromPosition.c_str())));
-					value--;
-					
-					//sprintf(startFromPosition.c_str(),"%d",value);
-					
-					sprintf(tempString,"%d",value);
 
-					cout << "starting from: " << tempString << endl;
-					useString = actualString + startFrom + tempString;
 				}
-				else if((atoi(rec.matchesLeft.c_str())) >= 100)
+				else //if((atoi(rec.matchesLeft.c_str())) >= 100)
 				{
 					cout << "inside second condition" << endl;
 					amountOfMatchesToFetch = "100";
@@ -399,6 +498,7 @@ int main()
 					cout << "done making string" << endl;
 
 				}
+				/*
 				else
 				{
 					amountOfMatchesToFetch = "100";
@@ -408,15 +508,16 @@ int main()
 					
 					sprintf(tempString,"%d",value);
 					useString = actualString + startFrom + tempString;
+					
+					std::cout << "fart" << std::endl;
 				}
-			}
-			if (skip == 0)
-			{
-				break;
+				*/
 			}
 		}
 		
-
+		// test the mostSeen function
+		std::cout << "trying to test most seen" << endl;
+		games.mostSeen();
 
 		
 		
